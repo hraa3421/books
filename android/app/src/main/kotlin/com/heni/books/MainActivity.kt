85ae5be475e49ffc165cc80fbc517916ed00d448
hraa3421 <amaliaheni775@gmail.com>
2025-03-21 21:38:42 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/heni/books/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.heni.books
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Heni Rizki Amalia',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com'; // Perbaikan dari "authoirty" menjadi "authority"
+    const path = '/books/v1/volumes/3ixIDwAAQBAJ';
+    Uri url = Uri.https(authority, path); // Perbaikan dari Uri.http() menjadi Uri.https()
+    return http.get(url);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Back from the Future')),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(child: const Text('GO!'), onPressed: () {}),
+            const Spacer(),
+            Text(result),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
